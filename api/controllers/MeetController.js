46704ca9ca3a4d172b5565b9fc5672856a3efa74
Jordan Nolan <jordanwn@gmail.com>
2014-08-11 19:36:15 -0400

    adds meet model, meet/entries association

---
+++ api/controllers/MeetController.js
@@ -0,0 +1,11 @@
+/**
+ * MeetController
+ *
+ * @description :: Server-side logic for managing Meets
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
--- api/models/Meet.js
+++ api/models/Meet.js
@@ -1,5 +1,5 @@
 /**
-* SwimEntry.js
+* Meet.js
 *
 * @description :: TODO: You might write a short summary of how this model works and what it represents here.
 * @docs        :: http://sailsjs.org/#!documentation/models
@@ -8,17 +8,9 @@
 module.exports = {
 
   attributes: {
-    time: {
-      type: 'string'
-    },
-    swimEvent: {
-      model: 'swimevent'
-    },
-    swimmer: {
-      model: 'swimmer'
-    },
-    meet {
-      model: 'meet'
+    swimEntries: {
+      collection: 'swimentry',
+      via 'meet'
     }
   }
 };
